f00317a34790bf0ab488849e89be979522da289a
TinyCreator <dev.imagines510@passinbox.com>
2024-10-25 17:59:38 +0200

    Estado inicial

--- slots.js
+++ slots.js
@@ -1,103 +1,94 @@
-const slotMachine = document.querySelector('.slot-machine');
-const spinButton = document.querySelector('.spin-button');
-const winMessage = document.querySelector('.win-message');
+//Declaración de elementos do HTML
+const slotMachine = document.querySelector('#slot-machine')
+const spinButton = document.querySelector('#spin-button')
+const winMessage = document.querySelector('.win-message')
+  //Carretes da tragaperras
+  const leftReel = document.querySelector('#reel1')
+  const middleReel = document.querySelector('#reel2')
+  const rightReel = document.querySelector('#reel3')
+    //Elementos de cada carrete
+    const topLeft = leftReel.querySelector('.top')
+    const midLeft = leftReel.querySelector('.center')
+    const botLeft = leftReel.querySelector('.bottom')
+    const topMiddle = middleReel.querySelector('.top')
+    const midMiddle = middleReel.querySelector('.center')
+    const botMiddle = middleReel.querySelector('.bottom')
+    const topRight = rightReel.querySelector('.top')
+    const midRight = rightReel.querySelector('.center')
+    const botRight = rightReel.querySelector('.bottom')
 
+
+//Simbolos dispoñibles
 const symbols = [
-  'Cherry', 
-  'Lemon', 
-  'Orange', 
-  'Plum', 
-  'Watermelon', 
-  'Grapes', 
-  'Seven', 
-  'Bell', 
-  'Bar'
+  'cherry', 
+  'lemon', 
+  'orange', 
+  'plum', 
+  'watermelon', 
+  'grapes', 
+  'seven', 
+  'bell', 
+  'bar'
 ];
 
-function slotElement(symbol){
-  this.symbol = symbol
+//Selección dun simbolo aleatorio 
+function randomSymbol(){
+  return Math.floor(Math.random() * symbols.length)
+} 
 
-  this.generateImage = function () {
-    const newImg = document.createElement('img')
-    newImg.setAttribute('id', symbol)
-    console.log(newImg)
-    return newImg
+//Ocorre cando se actualiza a páxina ou se abre por primeira vez 
+function initialState(){
+  //Distribución da tragaperras 
+  const initialDistribution = [
+    [topLeft, midLeft, botLeft],
+    [topMiddle, midMiddle, botMiddle],
+    [topRight, midRight, botRight]
+  ]
+
+  //Selección aleatoria de simbolos ao inicio
+  for(let outerI = 0; outerI < initialDistribution.length; outerI++ ){
+    for(let innerI = 0; innerI < initialDistribution[outerI].length; innerI++){
+      initialDistribution[outerI][innerI].insertAdjacentHTML('beforeend', `<img class="symbol" src="./img/${symbols[randomSymbol()]}.png">`)
+    }
   }
 }
+initialState()
 
-const randomSymbol = Math.floor(Math.random() * symbols.length)
-
-const shuffleArray = array => {
+//Devolve unha lista cos simbolos nun orde aleatorio
+function shuffleArray(array){
   for (let i = array.length - 1; i > 0; i--) {
     const newIndex = Math.floor(Math.random() * (i + 1));
     const temp = array[i];
     array[i] = array[newIndex];
     array[newIndex] = temp;
   }
+  return array
 }
 
+//Crea un carrete mediante a unión de 3 listas aleatorias de simbolos
+const reelPattern = [shuffleArray(symbols).concat(shuffleArray(symbols)).concat(shuffleArray(symbols))] 
 
-const slotPattern = [
-  [...symbols],
-  [...symbols],
-  [...symbols],
-];
 
-let winningCombinations = [
-  ['Cherry', 'Cherry', 'Cherry'],
-  ['Lemon', 'Lemon', 'Lemon'],
-  ['Orange', 'Orange', 'Orange'],
-  ['Plum', 'Plum', 'Plum'],
-  ['Watermelon', 'Watermelon', 'Watermelon'],
-  ['Grapes', 'Grapes', 'Grapes'],
-  ['Seven', 'Seven', 'Seven'],
-  ['Bell', 'Bell', 'Bell'],
-  ['Bar', 'Bar', 'Bar']
-];
-
-function addImgs(){
-  const reel1 = document.querySelector('#reel1')
-  const reel2 = document.querySelector('#reel2')
-  const reel3 = document.querySelector('#reel3')
-
-  reel1.appendChild(slotElement(symbols[randomSymbol]))
-  reel2.appendChild(slotElement(symbols[randomSymbol]))
-  reel3.appendChild(slotElement(symbols[randomSymbol]))
+function spinReels() {
+  setTimeout(checkForWin, 1000);
 }
-
-//addImgs()
 
 spinButton.addEventListener('click', spinReels);
 
-function spinReels() {
-  reels.forEach((reel) => {
-    reel.classList.add('spinning');
-    setTimeout(() => {
-      reel.classList.remove('spinning');
-      let randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
-      reel.querySelector('.symbol').textContent = randomSymbol;
-    }, 2000);
-  });
-
-
-setTimeout(checkForWin, 1000);
-}
 
 function checkForWin() {
-  let reel1Symbol = reels[0].querySelector('.symbol').textContent;
-  let reel2Symbol = reels[1].querySelector('.symbol').textContent;
-  let reel3Symbol = reels[2].querySelector('.symbol');
+  console.table([
+    [topLeft.children, midLeft.children, botLeft.children],
+    [topMiddle.children, midMiddle.children, botMiddle.children],
+    [topRight.children, midRight.children, botRight.children]
+  ])
 
-  let reel3img = reel3Symbol.querySelector('.symbol1')
-
-  let winningCombination = winningCombinations.find((combination) => {
-    return combination[0] === reel1Symbol && combination[1] === reel2Symbol && combination[2] === reel3Symbol;
-  });
-
-  if (winningCombination) {
+  if((topLeft == topMiddle && topLeft == topRight) 
+    || (topMiddle == midMiddle && topMiddle == botMiddle) 
+    || (topRight == midRight && topRight == botRight)){
     winMessage.textContent = `You won! ${winningCombination[0]} x3`;
     winMessage.style.display = 'block';
-  } else {
+  }else {
     winMessage.textContent = 'Better luck next time!';
     winMessage.style.display = 'block';
   }
